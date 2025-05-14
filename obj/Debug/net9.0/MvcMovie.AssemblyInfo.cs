a8116edac09057f91d1f2db932a6a6036c3ed9e6
Riswan Gani <riswangani11@gmail.com>
2025-05-14 17:11:47 +0700

    filtering movie

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a980d17f8c8167222bf3941725c488258d4fbff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ced6dc67c3430d60f645ba8f5da29272e9871d0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
